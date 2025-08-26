120fb902764e939eb3433e4f5a75e9e4f1f5965f
s.wu <s.wu@criteo.com>
2025-08-26 15:08:14 -0400

    Standalone Video Package - Update To Incorporate Retailer OMID Scripts JIRA: OPS-1999695

--- examples/simple_npm_app/src/main.ts
+++ examples/simple_npm_app/src/main.ts
@@ -5,5 +5,13 @@ import { vastContent } from "./vast_content";
 criteoVideoPlayerFromContent(
   vastContent,
   "video-player-container",
-  "Criteo Video Player"
+  {
+    altText: "Criteo Video Player",
+    targetDimensions: {
+      width: 720,
+      height: 406
+    },
+    sessionClientUrl: "https://static.criteo.net/banners/js/omidjs/stable/omid-session-client-v1.js",
+    omWebUrl: "https://static.criteo.net/banners/js/omidjs/stable/omweb-v1.js"
+  }
 );
