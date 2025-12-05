32ac56eaf2894c023daeadd634d8c9b18adb9677
s.wu <s.wu@criteo.com>
2025-12-05 11:38:22 -0500

    [Criteo Video Ad Player] - Do not display video elements when media files from VAST XML object or elementId is missing JIRA: OPS-2316839

--- src/components/video/video-render.tsx
+++ src/components/video/video-render.tsx
@@ -7,6 +7,13 @@ export const renderVideo = async (
   elementId: string,
   options: VideoOptions
 ) => {
+
+  //Not to display a blank video and control element when media file or element ID is missing
+  if (vastInformation.mediaFiles.length === 0 || !elementId) {
+    console.warn("No media files found the VAST XML object or element ID is missing. Aborting Video rendering.");
+    return null;
+  }
+
   render(
     <Video options={options} vastInformation={vastInformation} />,
     document.getElementById(elementId)!
